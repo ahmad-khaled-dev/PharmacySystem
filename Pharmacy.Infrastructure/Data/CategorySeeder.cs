3ae8b61bfaed08015352f725ee3e53dba556cde2
Ahmad <Ahmad@DESKTOP-MVOD94J>
2025-08-05 13:06:03 +0300

    Add Sale (Invoice)

---
+++ Pharmacy.Infrastructure/Data/CategorySeeder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pharmacy.Core.Domain.Entities;
+using Pharmacy.Infrastructure.DbContext;
+
+public static class CategorySeeder
+{
+    public static async Task SeedCategoryAsync(IServiceProvider provider)
+    {
+        var db = provider.GetRequiredService<ApplicationDbContext>();
+
+        if (!db.ProductCategories.Any(c => c.Name == "Medicine"))
+        {
+            db.ProductCategories.Add(new ProductCategory
+            {
+                Name = "Medicine"
+            });
+
+            await db.SaveChangesAsync();
+        }
+    }
+}
--- Pharmacy.WebApi/Program.cs
+++ Pharmacy.WebApi/Program.cs
@@ -26,6 +26,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     await IdentityInitializer.SeedAdminUserAsync(services);
+    await CategorySeeder.SeedCategoryAsync(services);
 }
 
 // Configure the HTTP request pipeline.
